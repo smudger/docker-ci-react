938f1982636f7b6ffb56b31462ff901411774c1d
Louis Smith <smudger06@gmail.com>
2020-01-26 10:03:19 +0000

    add docker-compose

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there, Tess!
+          Hi there, Tessa!
         </p>
         <a
           className="App-link"
